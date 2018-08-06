a9e31489ab67bc354b5b01114c50cd27ec07dc1f
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2018-08-06 23:39:18 +0100

    WIP: spring boot admin version fix.

--- src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
@@ -1,10 +1,16 @@
 package com.example.springbootadmin;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
@@ -12,5 +18,13 @@ public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
